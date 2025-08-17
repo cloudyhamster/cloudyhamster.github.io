0c9dacebc44d21ee38e9efba6d6190850f23dc12
cloudyhamster <sam_hossler@outlook.com>
2025-08-16 23:15:28 -0400

    removed tracking print (dont wanna leak my .env key again)

--- src/App.jsx
+++ src/App.jsx
@@ -6,8 +6,6 @@ const GA_TRACKING_ID = import.meta.env.VITE_GA_TRACKING_ID;
 
 function App() {
   useEffect(() => {
-    console.log('attempting to initialize GA with Tracking ID:', GA_TRACKING_ID);
-
     if (GA_TRACKING_ID) {
       ReactGA.initialize(GA_TRACKING_ID);
       ReactGA.send({ hitType: "pageview", page: window.location.pathname });
