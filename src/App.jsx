c5c90da81c6bff9aa001275780e09dc8dc02eb8b
cloudyhamster <sam_hossler@outlook.com>
2025-08-16 22:41:19 -0400

    hid env key (whoops!) and setup auto deploy

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,15 @@
-import React from 'react';
-import HomePage from './pages/HomePage';
+import { useEffect } from 'react';
+import ReactGA from 'react-ga4';
+
+const GA_TRACKING_ID = import.meta.env.VITE_GA_TRACKING_ID;
 
 function App() {
+  useEffect(() => {
+    if (GA_TRACKING_ID) {
+      ReactGA.initialize(GA_TRACKING_ID);
+      ReactGA.send({ hitType: "pageview", page: window.location.pathname });
+    }
+  }, []);
+
   return <HomePage />;
 }
-
-export default App;
