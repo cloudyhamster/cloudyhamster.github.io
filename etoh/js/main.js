8e615c3861a2c65168887e4bdc1d5fa727706664
cloudyhamster <sam_hossler@outlook.com>
2025-11-09 17:43:40 -0500

    OMG FIXED BACKEND URL

--- etoh/js/main.js
+++ etoh/js/main.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const API_BASE_URL = 'http://127.0.0.1:5000';
+    const API_BASE_URL = 'https://etoh-thing.onrender.com';
     let completionChart = null;
     let currentView = 'chart';
 
